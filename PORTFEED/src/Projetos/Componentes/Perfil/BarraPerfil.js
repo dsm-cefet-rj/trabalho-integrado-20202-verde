a91ff376e1f503e3dbe4a41b09c94308263da2f3
Diogo Soler <79878761+DiogoConde@users.noreply.github.com>
2021-03-18 11:57:37 -0300

    Barra com useState e useEffect
    Barra de perfil alterada para alternar entre os diferentes topicos dentro do item "Projetos"

--- PORTFEED/src/Projetos/Componentes/Perfil/BarraPerfil.js
+++ PORTFEED/src/Projetos/Componentes/Perfil/BarraPerfil.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import '/home/runner/PORTFEED/src/App.css';
 
-//Projeto para a barra de guia da tela de feed 
-class BarraPerfil extends React.Component {
+//Projeto para a barra de guia da tela de perfil
+
+const Projeto = ['Todos' ,'Programação' , 'Música', 'Jogos', 'Design', 'Fotografia']
+
+export default function BarraPerfil(){
+    const [position, setPosition] = useState(0);
+    const [Projetos, setProjeto] = useState('');
+
+    const onPress = () => {    
+    const pos = position + 1 === Projeto.length 
+      ? 0 
+      : position + 1;
+    setPosition(pos);
+    }
+
+    useEffect(() => {
+    setProjeto(Projeto[position]);
+   }, [position]);
+  
+  
+
+
+    return (
+      <div className="borda">
+      <div className="barra">
+      <button className="button, col-xs-4" onClick={() => alert('Sobre Mim')}>
+        Sobre Mim
+      </button>
+      <button className="button, col-xs-4" onClick={() => alert('Favoritos')}>
+        Favoritos
+      </button>
+      <button className="button, col-xs-4 " onClick={onPress}>
+        Projetos <span> - {Projetos} </span>
+      </button>
+      </div>
+      </div>
+    );
+  
+}
+/*class BarraPerfil extends React.Component {
   
   render() {
     return (
@@ -36,4 +75,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-export default BarraPerfil;
+export default BarraPerfil;*/
