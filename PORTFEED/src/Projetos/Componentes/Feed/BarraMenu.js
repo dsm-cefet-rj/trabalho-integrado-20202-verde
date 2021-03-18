749373387aef754dac1adb4dec6b2e7e11c1b43c
Eduardo Moura <79063539+EduMoura-debug@users.noreply.github.com>
2021-03-18 14:51:51 -0300

    Adicionando aparte do UseState
    UseState de favorito ainda nao funciona

--- PORTFEED/src/Projetos/Componentes/Feed/BarraMenu.js
+++ PORTFEED/src/Projetos/Componentes/Feed/BarraMenu.js
@@ -1,38 +1,42 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from 'react';
 import '/home/runner/PORTFEED/src/App.css';
-import '/Feed.css';
+import './Feed.css';
 
-const categories = ['Code', 'Games', 'Desing', 'Fotografia'];
 
-class BarraMenu extends React.Component {
-    
-handleClickTags(){
-  alert(categories);
-}
+const tag = ['Todas' ,'Code' , 'Game', 'Tech', 'Design', 'Fotografia','Musica']
 
-  render() {
+
+export default function BarraMenu(){
+
+  const [position, setPosition] = useState(0);
+  const [tags, setTag] = useState('');
+
+  const onPress = () => {    
+  const pos = position + 1 === tag.length 
+    ? 0 
+    : position + 1;
+  setPosition(pos);
+  }
+
+  useEffect(() => {
+  setTag(tag[position]);
+ }, [position]);
+
     return (
       <div className="borda">
       <div className="barra">
-      <button className="button, col-xs-4" onClick={() => alert('Novo')}>
-        Novo
+      <button className="button, col-xs-4" onClick={() => alert('Todos')}>
+        Todos
       </button>
-      <button className="button, col-xs-4" onClick={() => alert('Seguindo')}>
-        Seguindo
+      <button className="button, col-xs-4" onClick={() => alert('Favoritos')}>
+        Favoritos
       </button>
-      <button className="button, col-xs-4 " data-toggle="dropdown"  onClick={() => this.handleClickTags()}>
-        Tags <span class="caret"></span></button>
+      <button className="button, col-xs-4 " onClick={onPress}>
+        Tags <span > - {tags} </span></button>
       </div>
       </div>
     );
-  }
 }
 
 
-ReactDOM.render(
-  <BarraMenu />,
-  document.getElementById('root')
-);
-
-export default BarraMenu;
