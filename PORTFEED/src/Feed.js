e307b775b714c5f0ee4030b0524ae5ec42de59ed
Eduardo Moura <79063539+EduMoura-debug@users.noreply.github.com>
2021-04-08 10:45:48 -0300

    Feed Feito
    Parte de feed mapeada

--- PORTFEED/src/Feed.js
+++ PORTFEED/src/Feed.js
@@ -4,7 +4,7 @@ import Head from './Projetos/Componentes/Head.js';
 import BarraMenu from './Projetos/Componentes/Feed/BarraMenu.js'
 import FeedP from './Projetos/Componentes/Feed/Feed_Principal.js'
 import {Provider} from "react-redux";
-import store from './Projetos/Componentes/store/GuardaPerfil.js'
+import store from './Projetos/Componentes/store/GuardaProjeto.js'
 
 
 
--- PORTFEED/src/Projetos/Componentes/Feed/Feed_Principal.js
+++ PORTFEED/src/Projetos/Componentes/Feed/Feed_Principal.js
@@ -1,81 +1,115 @@
 
-import React, {useEffect} from 'react';
-import {useSelector, connect, useDispatch } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useParams, useHistory} from "react-router-dom"
+import {useSelector, connect, useDispatch} from 'react-redux';
 import {fetchProjetos, selectAllProjetos} from '../AddProjeto/SliceProjeto.js'
-
 import './Feed.css';
+
+/*
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
+*/
+
 import {
   Link
 } from "react-router-dom"
-/*
-function Feed(props) {
-  
-  
 
-  const status = useSelector(state => state.projetos.status);
-  const error = useSelector(state => state.projetos.error);
-  
-    
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    if (status === 'not_loaded' ) {
-        dispatch(fetchProjetos())
-    }else if(status === 'failed'){
-        setTimeout(()=>dispatch(fetchProjetos()), 5000);
+/*
+function Posts (props){
+
+    const projetos = useSelector(selectAllProjetos)
+    const status = useSelector(state => state.projetos.status)    
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      if (status === 'not_loaded') {
+          dispatch(fetchProjetos())
+      } 
+  }, [status, dispatch])
+
+      switch(status){
+        case 'loaded':
+            return(
+              <Feed/>
+              );
+        case 'failed':
+        default:
+            return (<div></div>)
     }
+}
+*/
+
+
+function Feed (props) {
+
+  const projetos = useSelector(selectAllProjetos)
+  const status = useSelector(state => state.projetos.status);
+  const error = useSelector(state => state.projetos.error);  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      if (status === 'not_loaded' || status === 'saved' || status === 'deleted') {
+          dispatch(fetchProjetos())
+      } 
   }, [status, dispatch])
   
-  
-  let ProjFeed = '';
+
+  let Feed = '';
   if(status === 'loaded' || status === 'saved' || status === 'deleted'){
-    ProjFeed = <FeedExibit projetos={projetos}/>;
+    Feed = <RenderPost projetos={projetos}/>;
   }else if(status === 'loading'){
-    ProjFeed = <div>Carregando Informações Extras...</div>;
+    Feed = <div>Carregando Feed...</div>;
   }else if(status === 'failed'){
-    ProjFeed = <div>Error: {error}</div>;
+    Feed = <div>Error: {error}</div>;
+  }
+
+ 
+          return(
+            <div className = "container" id="projetos">
+               <div className = "container" id="projetos">
+               <Link to = "/Novo">
+                <input type="submit" value="CRIAR NOVO" name = 'salva' />
+                </Link> 
+               {Feed}
+               </div>
+            </div>
+          );
+      
+  
   }
 
 
-     return( <>
-          {ProjFeed}
-   </>
-   );
-}
-
-*/
-
-function add()
-{
-     document.documentElement.scrollTop = 0
-}
-
-function Feed(props){
   
-  return(
-  
-    
-    <div className = "container">
-      
-        <div className = "li">
-        <div className="caixa">
-            <Link to = {`/Projeto/0`}>
-            <img className="img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFmwcSOL0Y2LvXIqo3YQEgK4MmGJYtVokRtw&usqp=CAU " alt="P2"/>  
-            </Link>
-            <Link to = {`/Projeto/1`}>
-            <img className="img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFmwcSOL0Y2LvXIqo3YQEgK4MmGJYtVokRtw&usqp=CAU " alt="P2"/>  
-            </Link>
-            
-            
-            </div>
+  function Post (props){
+    let { id } = useParams();
+    id = parseInt(id);
+
+    return(
+      <div className="li">
+        <div className="caixa" >
+       
+         <img className="img-responsive" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFmwcSOL0Y2LvXIqo3YQEgK4MmGJYtVokRtw&usqp=CAU "  alt= "P2"/>  
+         <p> {props.projeto.nome} </p>
+          
+          </div>
+          </div>
+    )
+
+  }
+function RenderPost(props){    
+    return(
+      <div className = "container" id="projetos">
+        {props.projetos.map((projeto) => <Link to = {`/Projeto/${projeto.id}`}><Post key={projeto.id} projeto={projeto}/></Link>)}
         </div>
-        <Link to = "/Novo">
-        <input type="submit" value="CRIAR NOVO" name = 'salva' Onclick = {() =>add()} />
-        </Link>
-    </div>
+      
+    )
 
-  );
 }
+//<Link to = {`/Projeto/${props.projetos[id].id}`}>
+//</Link> 
 
 
 export default connect(state => ({ projetos : state }))(Feed);
